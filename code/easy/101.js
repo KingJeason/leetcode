ca513d91049873b104c9dec3afe0dbc6900587fc
KingJeason <1503268691@qq.com>
2018-10-18 00:03:26 +0800

    101

--- code/easy/101.js
+++ code/easy/101.js
@@ -9,7 +9,35 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-
+var tree = {
+    val: 1,
+    left: {
+        val: 2,
+        left: {
+            val: 3,
+            left: null,
+            right: null,
+        },
+        right: {
+            val: 4,
+            left: null,
+            right: null,
+        }
+    },
+    right: {
+        val: 2,
+        left: {
+            val: 4,
+            left: null,
+            right: null
+        },
+        right: {
+            val: 3,
+            left: null,
+            right: null
+        }
+    }
+}
 var isSymmetric = function (root) {
     if (!root) return true;
     return is(root.left, root.right)
@@ -17,5 +45,7 @@ var isSymmetric = function (root) {
 var is = function (left, right) {
     if (!left && !right) return true;
     if ((!left && right) || (left && !right) || (left.val !== right.val)) return false;
-    return is((left.left, right.right) && (left.right, right.left))
+    console.log(left.left, right.right)
+    return is(left.left, right.right) && is(left.right, right.left)
 }
+console.log(isSymmetric(tree))
