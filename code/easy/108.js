01edd2ed813ebb1482ad1de5d19c632cb5f2a64c
KingJeason <1503268691@qq.com>
2018-10-21 23:25:26 +0800

    108 559

--- code/easy/108.js
+++ code/easy/108.js
@@ -5,10 +5,20 @@
  *     this.left = this.right = null;
  * }
  */
+function TreeNode (val) {
+    this.val = val;
+    this.left = this.right = null;
+}
 /**
  * @param {number[]} nums
  * @return {TreeNode}
  */
 var sortedArrayToBST = function (nums) {
-
+    if (!nums || !nums.length) return null
+    let midIndex = ~~(nums.length / 2)
+    let root = new TreeNode(nums[midIndex])
+    root.left = sortedArrayToBST(nums.slice(0, midIndex))
+    root.right = sortedArrayToBST(nums.slice(midIndex + 1))
+    return root
 };
+console.log(sortedArrayToBST([-10, -3, 0, 5, 9]))
